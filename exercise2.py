055a4257b3cb09dc147e3dad168db4ac13c2aaeb
Owlteeth <github@owlteeth.com>
2023-09-07 19:40:50 +0100

    exercise 2 submitted

--- exercise2.py
+++ exercise2.py
@@ -1,63 +1,40 @@
-# so I need to detect when I reach a value I've already reached, and that's the cycle
-
 def to_base(number, base):
+    """Returns a string containing the integer mapped to a base. Only works up to base 10
+    but that's known good for this scenario.
+    Completed in 4 hrs, 8 mins, 36 secs (actually less as I stopped for dinner before submitting).
+    """
     result = ""
     while number:
         result += str(number % base)
         number //= base
-    return result or ""
+    return result
 
-def minion_step(n, k, b):
-    l_n = list(n)
-    l_y = sorted(l_n)
-    l_x = l_y[::-1]
-    d_x = int(''.join(l_x), b)
-    d_y = int(''.join(l_y), b)
-    d_z = d_x - d_y
-    z = to_base(d_z, b) # z is a atring
-    pad = "0" * (k - len(z))
-    z = pad + z
-    #z = ''.join(l_z)
-    print(n)
-    print(d_x)
-    print(d_y)
-    print(d_z)
-    print(z)
-    print(len(z))
-    print(pad)
-    #print(z)
-
-def minion_gen(n, k, b):
-    l_n = list(n)
+def minion_gen(first_id, base):
+    """Generates an arbitrary number of minion IDs per the provided algo; 
+    they will start looping after a certain point."""
+    size = len(first_id)
+    next_id = first_id
     while True:
-        l_y = sorted(l_n)
-        l_x = l_y[::-1]
-        d_x = int(''.join(l_x), b)
-        d_y = int(''.join(l_y), b)
-        d_z = d_x - d_y
-        z = to_base(d_z, b) # z is a atring
-        pad = "0" * (k - len(z))
-        z = pad + z
-        yield z
-        l_n = list(z)
-
-
-#minion_step('1211',4,10)
-#minion_step('210022',6,3)
-#minion_step('210111',6,3)
-#g = minion_gen('210111',6,3)
-#for i in range(0,4):
-#    print(next(g))
+        list_y = sorted(list(next_id))
+        list_x = list_y[::-1]
+        x = int(''.join(list_x), base)
+        y = int(''.join(list_y), base)
+        z = x - y
+        next_id = to_base(z, base)
+        padding = "0" * (size - len(next_id))
+        next_id = padding + next_id
+        yield next_id
 
 def solution(n, b):
-    # remember n is a string!
-    k = len(n)
+    """Implements the function specced in hey-i-already-did-that (second challenge on https://foobar.withgoogle.com/)
+    Given a minion ID as a string n representing a nonnegative integer of length k in base b, where 2 <= k <= 9 and 2 <= b <= 10, write a function solution(n, b) which returns the length of the ending cycle of the algorithm above starting with n."""
+    # collect generated IDs with their generation number in a dict
     output = {}
-    for i, id in enumerate(minion_gen(n,k,b)):
-        if id in output:
-            j = output[id]
-            return i - j
+    for i, id in enumerate(minion_gen(n,b)):
+        past_i = output.get(id, None)
+        if past_i:
+            return i - past_i
         output[id] = i
 
-print(solution('210022', 3))
-print(solution('1211', 10))
+assert solution('1211', 10) == 1
+assert solution('210022', 3) == 3
